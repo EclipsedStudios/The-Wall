7e630d8ead15d34d60d51b28dba40035db1897ef
Jaden <contact@eclipsed-studios.com>
2020-12-07 12:36:49 -0500

    Fixed profile edits to show globally

--- CS Project/src/SocialMedia/GUIs/SocialProfileGUI.java
+++ CS Project/src/SocialMedia/GUIs/SocialProfileGUI.java
@@ -123,36 +123,6 @@ public class SocialProfileGUI extends JFrame implements ActionListener {
             }
         }
 
-        refreshButton = new JButton("Refresh Page");
-        refreshButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    UserInput.userClient.refreshPage();
-                    GUIProfile = UserClient.profile;
-                    nameLabel = new JLabel("          Name: " + GUIProfile.getName());
-                    //Print the username label with the username entered from logging in
-                    usernameLabel = new JLabel("          Username: " + UserInput.getUsernameAndPassword()[0]);
-                    //Age label
-                    ageLabel = new JLabel("          Age: " + GUIProfile.getAge());
-                    //Email label
-                    emailLabel = new JLabel("          Email: " + GUIProfile.getEmail());
-                    //Website label
-                    websiteLabel = new JLabel("          Website: " + GUIProfile.getWebsite());
-                    //Likes/Interests label
-                    likesInterestsLabel = new JLabel("          Likes/Interests: " + GUIProfile.getInterests());
-                    //Friends label
-                    friendsLabel = new JLabel("          Friends: ");
-                    //About Me label
-                    aboutMeLabel = new JLabel("          About Me:");
-                    aboutMeText = new JLabel("          " + GUIProfile.getAboutMe());
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
-        });
-
-        profileGUISouthComponentPanel.add(refreshButton);
-
         //Add 10 spaces before labels
         nameLabel = new JLabel("          Name: " + GUIProfile.getName());
         //Print the username label with the username entered from logging in
